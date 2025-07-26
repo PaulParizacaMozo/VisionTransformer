ac02136011982953959c5ee8b0f758c26e4b6b7c
nelinux <nelinux@linux.com>
2025-07-26 10:32:33 -0500

    create Visualizador class and attributes

---
+++ include/Visualizer.hpp
@@ -0,0 +1,27 @@
+#ifndef VISUALIZADOR_H
+#define VISUALIZADOR_H
+
+#include "core/Tensor.hpp"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+class Visualizador {
+public:
+    Visualizador(int ancho = 640, int alto = 480);
+    ~Visualizador();
+    
+    bool inicializarCamara();
+    Tensor capturarImagen();
+    Tensor predecir(const Tensor& entrada);
+    void ejecutar();
+
+private:
+    GLFWwindow* ventana;
+    int ancho_ventana;
+    int alto_ventana;
+    cv::VideoCapture camara;
+    
+    static void callbackTeclado(GLFWwindow* ventana, int tecla, int scancode, int accion, int mods);
+};
+
+#endif
