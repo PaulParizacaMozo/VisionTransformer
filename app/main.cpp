c8c0503e5d9570ebb82d2ceb5f18e18cd0cb2276
i-am-sergio <sergio15.dev@gmail.com>
2025-07-27 09:04:05 -0500

    feat: fix DataAugmentation with rotation traslate y zoom transformation

--- app/main.cpp
+++ app/main.cpp
@@ -9,17 +9,17 @@ int main() {
   try {
     // --- 1. Definir Configuraciones ---
     ViTConfig model_config;
-    model_config.embedding_dim = 64; // 196 
-    model_config.num_layers = 2; // 6 
-    model_config.num_heads = 4;
-    model_config.patch_size = 7;
+    model_config.embedding_dim = 128; // 196 
+    model_config.num_layers = 8; // 6 
+    model_config.num_heads = 16;
+    model_config.patch_size = 14;
     model_config.num_classes = 10;
     model_config.in_channels = 1;
     model_config.mlp_hidden_dim = model_config.embedding_dim * 4;
     model_config.dropout_rate = 0.2;
 
     TrainerConfig train_config;
-    train_config.epochs = 30;
+    train_config.epochs = 20;
     train_config.batch_size = 64;// 128
     train_config.learning_rate = 3e-4f;
     train_config.weight_decay = 1e-4f; // 0.01f
@@ -33,7 +33,7 @@ int main() {
     // Entrenamiento + validación
     auto [train_data, valid_data] =
     load_csv_data_train_val("data/mnist_train.csv",
-                            1.0f,   // sample_frac   → 25 % del total
+                            0.5f,   // sample_frac   → 25 % del total
                             0.80f,   // train_frac    → 80 % de ese 30%
                             0.20f,   // val_frac      → 20 % de ese 30%
                             1,
